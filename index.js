7fad925370396e13384d22e846a887901e44642d
bjellesma <william.jellesma@gmail.com>
2019-11-12 23:29:41 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const redis = require('redis')
+
+const PORT = process.env.PORT || 5000
+const REDIS_PORT = process.env.PORT || 6379
+
+const client = redis.createClient(REDIS_PORT)
+
+const app = express()
+
+function setResponse(username, repos){
+    return `<h2>${username} has ${repos} repos</h2>`
+}
+
+async function getRepos(req, res, next){
+    try{
+        console.log('fetch data')
+
+        const {username} = req.params
+
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        //wait for data to be received
+        const data = await response.json();
+
+        const repos = data.public_repos
+        //set to redis
+        //set key value pair of username:repos
+        client.setex(username, 3600, repos)
+
+        res.send(setResponse(username, repos))
+    } catch(err){
+        console.error(err)
+        res.status(500)
+    }
+}
+
+// Cache middleware
+// middleware just runs between the request/response cycle
+
+function cache(){
+    const {username} = req.params 
+    client.get('username', (err, data) => {
+        if(err){
+            throw err 
+        }
+        if(data !== null){
+            res.send(setResponse(username, data));
+        }else{
+            next()
+        }
+    })
+}
+
+app.get('/repos/:username', getRepos)
+
+app.listen(5000, () => {
+    console.log('server running')
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Linux install
+
+1. `sudo apt-get install redis-server`
+2. create package.json with `npm init -y`
+3. install express, fetch, and the redis driver with `npm i express node-fetch redis`
+4. add nodemon with `npm i -D nodemon`
